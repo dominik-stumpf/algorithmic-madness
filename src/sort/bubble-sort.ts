acebc9a6dccc4184b10843cc1271aa838456bab3
Dominik Stumpf <s.dominik.work@gmail.com>
2023-11-28 20:57:17 +0100

    feat: add bubble sort

---
+++ src/sort/bubble-sort.ts
@@ -0,0 +1,12 @@
+export function bubbleSort(arr: number[]): number[] {
+  for (let i = 0; i < arr.length - 1; i += 1) {
+    for (let j = 0; j < arr.length - i - 1; j += 1) {
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
---
+++ src/sort/sort.test.ts
@@ -0,0 +1,34 @@
+import { bubbleSort } from './bubble-sort';
+import { describe, it, expect } from 'bun:test';
+
+describe('bubbleSort', () => {
+  it('should correctly sort an array of positive numbers in ascending order', () => {
+    const unsortedArray = [5, 3, 8, 4, 2];
+    const sortedArray = bubbleSort([...unsortedArray]);
+    expect(sortedArray).toEqual([2, 3, 4, 5, 8]);
+  });
+
+  it('should correctly sort an array of whole numbers in ascending order', () => {
+    const unsortedArray = [0, -1, 5, 3, -43, 8, 4, 2];
+    const sortedArray = bubbleSort([...unsortedArray]);
+    expect(sortedArray).toEqual([-43, -1, 0, 2, 3, 4, 5, 8]);
+  });
+
+  it('should correctly sort an array of whole repeating numbers in ascending order', () => {
+    const unsortedArray = [0, -1, 5, 0, 0, 3, -43, 8, 4, 2, 0, 2];
+    const sortedArray = bubbleSort([...unsortedArray]);
+    expect(sortedArray).toEqual([-43, -1, 0, 0, 0, 0, 2, 2, 3, 4, 5, 8]);
+  });
+
+  it('should handle an empty array', () => {
+    const emptyArray: number[] = [];
+    const sortedArray = bubbleSort([...emptyArray]);
+    expect(sortedArray).toEqual([]);
+  });
+
+  it('should handle an array with one element', () => {
+    const singleElementArray = [42];
+    const sortedArray = bubbleSort([...singleElementArray]);
+    expect(sortedArray).toEqual([42]);
+  });
+});
