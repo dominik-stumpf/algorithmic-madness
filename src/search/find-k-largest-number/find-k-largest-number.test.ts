21b8bf49e30792203fdbedcf6c676fa728af8266
Dominik Stumpf <s.dominik.work@gmail.com>
2023-12-01 19:09:44 +0100

    refactor: changed case for subarray

--- src/search/find-k-largest-number/find-k-largest-number.test.ts
+++ src/search/find-k-largest-number/find-k-largest-number.test.ts
@@ -9,36 +9,36 @@ for (const [algorithmName, algorithm] of Object.entries(algorithms)) {
   describe(algorithmName, () => {
     it('should find the k largest element', () => {
       const unsortedArray = [2, 4, 211, 1, 0, 0, 88493];
-      const subArrayLength = 3;
+      const subarrayLength = 3;
 
-      expect(sortAscending(algorithm(unsortedArray, subArrayLength))).toEqual([
+      expect(sortAscending(algorithm(unsortedArray, subarrayLength))).toEqual([
         4, 211, 88493,
       ]);
     });
 
     it('should handle empty array', () => {
       const unsortedArray: number[] = [];
-      const subArrayLength = 3;
+      const subarrayLength = 3;
 
-      expect(sortAscending(algorithm(unsortedArray, subArrayLength))).toEqual(
+      expect(sortAscending(algorithm(unsortedArray, subarrayLength))).toEqual(
         [],
       );
     });
 
     it('should handle k as zero', () => {
       const unsortedArray: number[] = [3290, 43333, 0, 0, -342];
-      const subArrayLength = 0;
+      const subarrayLength = 0;
 
-      expect(sortAscending(algorithm(unsortedArray, subArrayLength))).toEqual(
+      expect(sortAscending(algorithm(unsortedArray, subarrayLength))).toEqual(
         [],
       );
     });
 
     it('should handle larger k than array length', () => {
       const unsortedArray: number[] = [3290, 43333, 0, 0, -342];
-      const subArrayLength = 9;
+      const subarrayLength = 9;
 
-      expect(sortAscending(algorithm(unsortedArray, subArrayLength))).toEqual(
+      expect(sortAscending(algorithm(unsortedArray, subarrayLength))).toEqual(
         sortAscending(unsortedArray),
       );
     });
